40d981aeab5455fdf7800f26e18b94d8be3f1d3a
Rich <rhildred@conestogac.ca>
2023-07-13 09:52:52 -0400

    add view for home page

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 
 let app = express();
 
+// static folder
 app.use(express.static("public"));
+// views folder
+app.set("views", "views");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>{
-    res.send("hello from express");
+    //res.send("hello from express");
+    res.render("home");
 });
 
 const server = app.listen(8080, ()=>{
