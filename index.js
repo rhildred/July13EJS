292d63e43a319036536cc87eb02297a78f82870d
Rich <rhildred@conestogac.ca>
2023-07-13 09:31:07 -0400

    add express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Starting from scratch");
+const express = require("express");
+
+let app = express();
+
+app.get("/", (req, res)=>{
+    res.send("hello from express");
+});
+
+const server = app.listen(8080, ()=>{
+    console.log(`Starting from scratch on port ${server.address().port}`);
+});
+
